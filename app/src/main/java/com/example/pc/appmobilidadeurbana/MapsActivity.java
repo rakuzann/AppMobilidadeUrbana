4133b25973e267989321b1c51d5fe3134415f53d
Antonio <antoniopegas@hotmail.com>
2018-06-20 00:20:07 +0100

    Cenas Random 2

--- app/src/main/java/com/example/pc/appmobilidadeurbana/MapsActivity.java
+++ app/src/main/java/com/example/pc/appmobilidadeurbana/MapsActivity.java
@@ -31,8 +31,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng casa  = new LatLng(-39.825350, -7.507580);
+        mMap.addMarker(new MarkerOptions().position(casa).title("Estou te a ver fdep joao"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(casa));
     }
 }
