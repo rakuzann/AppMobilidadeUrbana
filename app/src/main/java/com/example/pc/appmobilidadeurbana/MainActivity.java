3578b5a249dc13de5312001e9705311f1f62933c
Antonio <antoniopegas@hotmail.com>
2018-06-20 00:02:27 +0100

    Intent para o mapa

--- app/src/main/java/com/example/pc/appmobilidadeurbana/MainActivity.java
+++ app/src/main/java/com/example/pc/appmobilidadeurbana/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.pc.appmobilidadeurbana;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+
+    public void btnStart (View v){
+        Intent mudar = new Intent(this, MapsActivity.class);
+        startActivity(mudar);
     }
 }
