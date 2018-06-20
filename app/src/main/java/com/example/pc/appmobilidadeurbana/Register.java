216a12dc7c0270c6875cba768aacac96833ca9e9
joao <joao_jcas@hotmail.com>
2018-06-20 23:20:21 +0100

    register not working

--- app/src/main/java/com/example/pc/appmobilidadeurbana/Register.java
+++ app/src/main/java/com/example/pc/appmobilidadeurbana/Register.java
@@ -1,12 +1,15 @@
 package com.example.pc.appmobilidadeurbana;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.pc.appmobilidadeurbana.objetos.Utilizador;
 import com.example.pc.appmobilidadeurbana.objetos.server;
 
 public class Register extends AppCompatActivity {
@@ -30,13 +33,19 @@ public class Register extends AppCompatActivity {
 
     public void registo(View view){
 
-        String user = username.getText().toString();
-        String pw = password.getText().toString();
-        String nm = nome.getText().toString();
-        String mail = email.getText().toString();
+        new Thread(){
+            public void run (){
+                String user = username.getText().toString();
+                String pw = password.getText().toString();
+                String nm = nome.getText().toString();
+                String mail = email.getText().toString();
 
+                server.postRegisterHttp(user,pw,nm,mail);
 
-        server.postRegisterHttp(user,pw,nm,mail);
+            }
+        }.start();
+
     }
-
 }
+
+
