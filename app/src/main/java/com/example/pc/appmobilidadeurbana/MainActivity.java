b3ae0d24d4d65e42f302af91f85e4f9c8543f978
joao <joao_jcas@hotmail.com>
2018-06-21 05:37:10 +0100

    navigation drawer done ( sem onclick)

--- app/src/main/java/com/example/pc/appmobilidadeurbana/MainActivity.java
+++ app/src/main/java/com/example/pc/appmobilidadeurbana/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.pc.appmobilidadeurbana;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -8,10 +11,11 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.pc.appmobilidadeurbana.objetos.Utilizador;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     TextView textViewUser;
 
@@ -36,13 +40,38 @@ public class MainActivity extends AppCompatActivity {
         //------------- cenas da action bar -------------------
 
         mDrawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         nToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
 
         mDrawerLayout.addDrawerListener(nToggle);
         nToggle.syncState();
+    }
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_map:
+                Toast.makeText(this, "nav_map", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_account:
+                //Intent i = new Intent(this, Register.class);
+                //startActivity(i);
+                Toast.makeText(this, "nav_account", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_settings:
+                Toast.makeText(this, "nav_settings", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_taxi:
+                Toast.makeText(this, "nav_taxi", Toast.LENGTH_SHORT).show();
+                break;
 
+        }
+
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 
     @Override
@@ -53,4 +82,8 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void btnTeste(View view){
+        Intent mudar = new Intent(this, testes.class);
+        startActivity(mudar);
+    }
 }
