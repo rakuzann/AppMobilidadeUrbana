eebe4c46d9bc587b590b8f607a1dfad464bb4d23
joao <joao_jcas@hotmail.com>
2018-06-21 02:31:07 +0100

    navigation drawer done ( sem onclick)

--- app/src/main/java/com/example/pc/appmobilidadeurbana/MainActivity.java
+++ app/src/main/java/com/example/pc/appmobilidadeurbana/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.pc.appmobilidadeurbana;
 
+import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import com.example.pc.appmobilidadeurbana.objetos.Utilizador;
@@ -10,11 +15,15 @@ public class MainActivity extends AppCompatActivity {
 
     TextView textViewUser;
 
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle nToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //---------------saber qual o user que está logged in --------------
         textViewUser = findViewById(R.id.textViewUser);
 
         Utilizador utilizador = (Utilizador) getIntent().getSerializableExtra("utilizador");
@@ -23,5 +32,25 @@ public class MainActivity extends AppCompatActivity {
             textViewUser.setText(utilizador.getNome().toString());
         else
             textViewUser.setText("guest");
+
+        //------------- cenas da action bar -------------------
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        nToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
+
+        mDrawerLayout.addDrawerListener(nToggle);
+        nToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(nToggle.onOptionsItemSelected(item))
+            return true;
+        return super.onOptionsItemSelected(item);
+    }
+
 }
